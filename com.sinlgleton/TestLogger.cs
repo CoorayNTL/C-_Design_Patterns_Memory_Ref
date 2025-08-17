d16f8fbaf7f9f29a3365cedf7bd351565b394150
CoorayNTL <tl685998@gmail.com>
2025-08-17 09:50:35 +0530

    Add project files.

---
+++ com.sinlgleton/TestLogger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.com.sinlgleton
+{
+    class TestLogger
+    {
+       
+        static void Main()
+        {
+            // Logger obj1 =  new Logger;
+            //Logger obj2 = new Logger();
+
+            Logger obj1 = Logger.GetInstance();
+            Logger obj2 = Logger.GetInstance();
+
+            Console.WriteLine(obj1.GetHashCode());
+            Console.WriteLine(obj2.GetHashCode());
+
+            Test obj3 = new Test();
+            Test obj4 = new Test();
+            Console.WriteLine(obj3.GetHashCode());
+            Console.WriteLine(obj4.GetHashCode());
+
+        }
+
+        class Test
+        {
+
+        }
+       
+
+    }
+}
